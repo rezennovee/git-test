ddce6b5bc7ebe048c4036dfd3f5ba9d831625251
crist <122@jiaqi.com>
2021-11-21 01:15:30 +0800

    third version

--- src/main/java/com/apache/jiaqi/Controller.java
+++ src/main/java/com/apache/jiaqi/Controller.java
@@ -3,5 +3,6 @@ package com.apache.jiaqi;
 public class Controller {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
     }
 }
